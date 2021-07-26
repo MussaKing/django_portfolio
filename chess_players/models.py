6ed596f6c47ea6312e5982bbe7e434dce0be73d0
MussaKing <mussa.overlord@gmail.com>
2021-07-26 04:44:48 +0300

    add migration and connect to DB SQLite

--- chess_players/models.py
+++ chess_players/models.py
@@ -1,3 +1,10 @@
 from django.db import models
 
-# Create your models here.
+class Player(models.Model):
+    first_name = models.CharField(max_length=255)
+    second_name = models.CharField(max_length=255)
+    middle_name = models.CharField(max_length=255)
+    date_of_birth = models.DateTimeField()
+    date_of_death = models.DateTimeField()
+    content = models.TextField(blank = True)
+    photo = models.ImageField(upload_to="photos/%Y/%m/%d/")
--- django_portfolio/urls.py
+++ django_portfolio/urls.py
@@ -13,9 +13,11 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+from django.conf.urls.static import static
 from django.contrib import admin
 from django.urls import include, path
 from chess_players.views import main, pageNotFound
+from django_portfolio import settings
 
 
 urlpatterns = [
@@ -23,4 +25,7 @@ urlpatterns = [
     path('', main, name='main'),
     path('players/', include('chess_players.urls'))
 ]
+
+if settings.DEBUG:
+    urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 handler404 = pageNotFound
